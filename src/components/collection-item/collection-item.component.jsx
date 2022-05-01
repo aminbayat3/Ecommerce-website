9549ef1bba5b09b6406664f2f28a88fe8de12469
aminbayat3 <aminglbayatpour3@gmail.com>
2022-04-30 21:29:43 -0700

    updated cart utils to group item in cart reducer

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import CustomButton from '../custom-button/custom-button.component';
+import { addItem } from '../../redux/cart/cart.actions';
 
 import './collection-item.styles.scss';
 
-const onHandleClick = () => {
-    console.log('CollectionItemButton Clicked!!');
-}
 
-const CollectionItem = ({ name, imageUrl, price }) => {
+const CollectionItem = ({ item, cartItems}) => {
+    const { name, imageUrl, price } = item;
+
+    const onHandleClick = () => {
+        cartItems(item);
+    }
+
     return(
         <div className="collection-item">
             <div className='image' style={{
@@ -19,9 +24,14 @@ const CollectionItem = ({ name, imageUrl, price }) => {
                 <span className='price'>{price}</span>
             </div>
 
-            <CustomButton onHandleClick={onHandleClick}>Add item to cart </CustomButton> 
+            <CustomButton onHandleClick={onHandleClick} inverted>Add item to cart </CustomButton> 
         </div>
     )
 }
+
+const mapDispatchToProps = (dispatch) => ({
+    cartItems: (item) => dispatch(addItem(item))
+});
+
 // we cannot use onClick on a component, so instead we can define a ClickHandler function with the same name to remedy this problem
-export default CollectionItem;
+export default connect(null, mapDispatchToProps)(CollectionItem);
--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 import "./custom-button.styles.scss";
 
-const CustomButton = ({ children, type, isGoogleSignIn, onHandleClick }) => {
+const CustomButton = ({ children, type, isGoogleSignIn, inverted, onHandleClick }) => {
   return (
     <button
       type={type}
-      className={`${isGoogleSignIn ? "google-sign-in" : ""} custom-button`} onClick={() => onHandleClick ? onHandleClick() : null}
+      className={`${inverted ? 'inverted' : ''} ${isGoogleSignIn ? "google-sign-in" : ""} custom-button`} onClick={() => onHandleClick ? onHandleClick() : null}
     >
       {children}
     </button>
